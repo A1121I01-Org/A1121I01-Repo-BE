968db68fda7a7dbe0b286fa9950f081272d3e952
phuongnhi1604 <phuongnhi1641@gmail.com>
2022-10-06 16:15:09 +0700

    NhiVP up set up account

--- backend/src/main/java/module6/backend/repository/IAccountRepository.java
+++ backend/src/main/java/module6/backend/repository/IAccountRepository.java
@@ -2,8 +2,11 @@ package module6.backend.repository;
 
 import module6.backend.entity.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IAccountRepository extends JpaRepository<Account, Long> {
+    @Query(value = "SELECT * FROM account WHERE username=?1", nativeQuery = true)
+    Account findAccountByUsername(String username);
 }
