39a00818297488143b448bf8fc149d5432bbbc35
phuongnhi1604 <phuongnhi1641@gmail.com>
2022-10-07 10:22:09 +0700

    NhiVP up create-account

--- backend/src/main/java/module6/backend/repository/IAccountRepository.java
+++ backend/src/main/java/module6/backend/repository/IAccountRepository.java
@@ -7,6 +7,13 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IAccountRepository extends JpaRepository<Account, Long> {
-    @Query(value = "SELECT * FROM account WHERE username=?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM account WHERE username=?1 and account_flag = 0", nativeQuery = true)
     Account findAccountByUsername(String username);
+
+    @Query(value = "INSERT INTO account(username,password) values (?1,?2)", nativeQuery = true)
+    void saveAccount(String username, String password);
+
+
+    @Query(value = "UPDATE account as a set a.username=?1, a.password=?2 where a.account_id=?3", nativeQuery = true)
+    Account updateAccount(String username, String password, Long accountId);
 }
--- backend/src/main/java/module6/backend/repository/IEmployeeRepository.java
+++ backend/src/main/java/module6/backend/repository/IEmployeeRepository.java
@@ -2,8 +2,22 @@ package module6.backend.repository;
 
 import module6.backend.entity.employee.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
 
 @Repository
 public interface IEmployeeRepository extends JpaRepository<Employee, Long> {
+    //NhiVP code tim employee bang code
+    @Query(value = "SELECT * from employee where employee_code =?1 and employee_flag = 0", nativeQuery = true)
+    Employee findEmployeeByCode(String code);
+
+    //NhiVP code tim employee da co account
+    @Query(value = "select * from employee where employee_code =?1 and employee_account_id is not null and employee_flag = 0", nativeQuery = true)
+    Employee findExistEmployeeHasAccount(String code);
+
+    //NhiVP code tim employee chua co account
+    @Query(value = "select * from employee where employee_code =?1 and employee_account_id is null and employee_flag = 0", nativeQuery = true)
+    Employee findExistEmployeeDontHasAccount(String code);
 }
