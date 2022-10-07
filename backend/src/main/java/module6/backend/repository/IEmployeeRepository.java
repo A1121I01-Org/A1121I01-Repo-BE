3d77448b8550015cb44c287cff4ceb22cae40387
lehungson0510 <hungsonle0510@gmail.com>
2022-10-07 23:44:33 +0700

    SonLH up merge AnDVH and SonLH

--- backend/src/main/java/module6/backend/repository/IEmployeeRepository.java
+++ backend/src/main/java/module6/backend/repository/IEmployeeRepository.java
@@ -2,8 +2,10 @@ package module6.backend.repository;
 
 import module6.backend.entity.employee.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.Optional;
@@ -13,7 +15,8 @@ public interface IEmployeeRepository extends JpaRepository<Employee, Long> {
     @Query(value = "select * from employee where employee_id = ?1 and employee_position_id > 0 and employee_account_id > 0 and employee_flag = 0 ", nativeQuery = true)
     Optional<Employee> findEmployeeById(Long id);
 
-
+    @Transactional
+    @Modifying
     @Query(value = "UPDATE employee SET employee_name = ?1, employee_avatar = ?2, employee_date_of_birth= ?3, employee_gender = ?4, employee_address = ?5, employee_phone = ?6 WHERE employee_id = ?7", nativeQuery = true)
     void updateEmployee(String employeeName, String employeeAvatar, LocalDate employeeDateOfBirth, String employeeGender, String employeeAddress, String employeePhone, Long employeeId);
 
