a082944d12f1b423cdaa2c2740eda6cc01d1c229
tanit2503 <congtan2503@gmail.com>
2022-10-07 15:01:40 +0700

    TanDC update LoginController

--- backend/src/main/java/module6/backend/controller/LoginController.java
+++ backend/src/main/java/module6/backend/controller/LoginController.java
@@ -1,11 +1,20 @@
 package module6.backend.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import module6.backend.payload.request.LoginRequest;
+import module6.backend.payload.response.JwtResponse;
+import module6.backend.service.Impl.JwtServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping("api/login")
 public class LoginController {
+	@Autowired
+	private JwtServiceImpl jwtService;
+
+	@PostMapping({"/authenticate"})
+	public JwtResponse createJwtToken(@RequestBody LoginRequest loginRequest) throws Exception {
+		return jwtService.createJwtToken(loginRequest);
+	}
 }
