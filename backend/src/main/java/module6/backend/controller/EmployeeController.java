b4c1c866079489c0d31b43fb20ac9c5c43e8b90f
dingan4109 <94068582+dingan4109@users.noreply.github.com>
2022-10-07 21:30:38 +0700

    20221007 0928pm  update employee done

--- backend/src/main/java/module6/backend/controller/EmployeeController.java
+++ backend/src/main/java/module6/backend/controller/EmployeeController.java
@@ -1,13 +1,17 @@
 package module6.backend.controller;
 
+import module6.backend.entity.employee.Employee;
 import module6.backend.service.IAccountService;
 import module6.backend.service.IEmployeeService;
 import module6.backend.service.IPositionService;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @CrossOrigin("*")
@@ -21,5 +25,30 @@ public class EmployeeController {
 
     @Autowired
     private IPositionService positionService;
+
+    @GetMapping("detail/{id}")
+    public ResponseEntity<Employee> findEmployeeById(@PathVariable("id") Long id) {
+        Employee employee = employeeService.findEmployeeById(id);
+        if(employee == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<Employee>(employee, HttpStatus.OK);
+    }
+
+    @PatchMapping("update/{id}")
+    public ResponseEntity<?> updateEmployee(@PathVariable("id") Long id, @Valid @RequestBody Employee employee, BindingResult bindingResult){
+        if(bindingResult.hasErrors()) {
+            return new ResponseEntity<>(HttpStatus.PRECONDITION_FAILED);
+        }
+
+        Employee foundEmployee = employeeService.findEmployeeById(id);
+        if(foundEmployee == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }else {
+            employeeService.updateEmployee(employee.getEmployeeName(),employee.getEmployeeAvatar(),employee.getEmployeeDateOfBirth(),employee.getEmployeeGender(),employee.getEmployeeAddress(),employee.getEmployeePhone(),id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+    }
 
 }
--- backend/src/main/java/module6/backend/repository/IEmployeeRepository.java
+++ backend/src/main/java/module6/backend/repository/IEmployeeRepository.java
@@ -2,8 +2,16 @@ package module6.backend.repository;
 
 import module6.backend.entity.employee.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
 
 @Repository
 public interface IEmployeeRepository extends JpaRepository<Employee, Long> {
+    @Query(value = "SELECT * FROM employee WHERE employee_id = ?1", nativeQuery = true)
+    Employee findEmployeeById(Long employeeId);
+
+    @Query(value = "UPDATE employee SET employee_name = ?1, employee_avatar = ?2, employee_date_of_birth= ?3, employee_gender = ?4, employee_address = ?5, employee_phone = ?6 WHERE employee_id = ?7", nativeQuery = true)
+    void updateEmployee(String employeeName, String employeeAvatar, LocalDate employeeDateOfBirth, String employeeGender, String employeeAddress, String employeePhone, Long employeeId);
 }
