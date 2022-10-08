7c84e283ea5daefeac713ed32d276d9d9606df1f
dingan4109 <94068582+dingan4109@users.noreply.github.com>
2022-10-08 21:13:17 +0700

    test only

--- backend/src/main/java/module6/backend/repository/IEmployeeRepository.java
+++ backend/src/main/java/module6/backend/repository/IEmployeeRepository.java
@@ -2,8 +2,10 @@ package module6.backend.repository;
 
 import module6.backend.entity.employee.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 
@@ -12,6 +14,8 @@ public interface IEmployeeRepository extends JpaRepository<Employee, Long> {
     @Query(value = "SELECT * FROM employee WHERE employee_id = ?1", nativeQuery = true)
     Employee findEmployeeById(Long employeeId);
 
+    @Transactional
+    @Modifying
     @Query(value = "UPDATE employee SET employee_name = ?1, employee_avatar = ?2, employee_date_of_birth= ?3, employee_gender = ?4, employee_address = ?5, employee_phone = ?6 WHERE employee_id = ?7", nativeQuery = true)
     void updateEmployee(String employeeName, String employeeAvatar, LocalDate employeeDateOfBirth, String employeeGender, String employeeAddress, String employeePhone, Long employeeId);
 }
