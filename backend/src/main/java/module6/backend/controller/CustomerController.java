d78a43a0330c025ad0aa372c033fa6dba5c775b2
trongduy282 <dinhtrantrongduy@gmail.com>
2022-10-08 19:56:21 +0700

    BE

--- backend/src/main/java/module6/backend/controller/CustomerController.java
+++ backend/src/main/java/module6/backend/controller/CustomerController.java
@@ -1,13 +1,16 @@
 package module6.backend.controller;
 
+import module6.backend.entity.customer.Customer;
 import module6.backend.service.ICustomerService;
 import module6.backend.service.ICustomerTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin("*")
@@ -19,4 +22,24 @@ public class CustomerController {
     @Autowired
     private ICustomerTypeService customerTypeService;
 
+    @GetMapping("customer-findById/{id}")
+    public ResponseEntity<Customer> findById(@PathVariable Long id) {
+        return new ResponseEntity<>(customerService.findCustomerById(id).get(), HttpStatus.OK);
+    }
+
+    @PostMapping("customer-create")
+    public ResponseEntity<Customer> createNewCustomer(@RequestBody Customer customer) {
+        customerService.createCustomer(customer.getCustomerName(),customer.getCustomerCode(),customer.getCustomerAvatar(),customer.getCustomerAddress(),customer.getCustomerPhone(),customer.getCustomerEmail(),customer.getCustomerTypeId().getCustomerTypeId());
+        return new ResponseEntity<>(customer, HttpStatus.CREATED);
+    }
+
+    @PatchMapping("update")
+    public ResponseEntity<Customer> updateCustomer(@RequestBody Customer customer) {
+        Optional<Customer> customerOptional = customerService.findCustomerById(customer.getCustomerId());
+        if (!customerOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        customerService.updateCustomer(customer.getCustomerName(), customer.getCustomerCode(), customer.getCustomerAvatar(), customer.getCustomerAddress(), customer.getCustomerPhone(), customer.getCustomerEmail(), customer.getCustomerTypeId().getCustomerTypeId(), customer.getCustomerId());
+        return new ResponseEntity<>(customer,HttpStatus.OK);
+    }
 }
--- backend/src/main/java/module6/backend/service/ICustomerService.java
+++ backend/src/main/java/module6/backend/service/ICustomerService.java
@@ -1,4 +1,14 @@
 package module6.backend.service;
 
+import module6.backend.entity.customer.Customer;
+
+import java.util.Optional;
+
 public interface ICustomerService {
+    //DuyDTT code save và findCustomerById
+    Customer save(Customer customer);
+//    Optional<Customer> findCustomerById(Long id);
+    void updateCustomer(String name,String code,String avatar,String address,String phone,String email,Long typeId, Long id);
+    Optional<Customer> findCustomerById(Long id);
+    void createCustomer(String name, String code, String avt, String address, String phone, String email, Long customerType);
 }
