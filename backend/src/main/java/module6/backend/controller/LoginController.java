61d3a777cc4e0515d34959fc11e1cb4e6d4f23c1
tanit2503 <congtan2503@gmail.com>
2022-10-11 22:52:22 +0700

    Update

--- backend/src/main/java/module6/backend/controller/LoginController.java
+++ backend/src/main/java/module6/backend/controller/LoginController.java
@@ -1,11 +1,21 @@
 package module6.backend.controller;
 
+import module6.backend.payload.request.LoginRequest;
+import module6.backend.payload.response.JwtResponse;
+import module6.backend.security.jwt.JwtServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin("*")
-@RequestMapping("api/login")
+@CrossOrigin
+//@RequestMapping("auth/login")
 public class LoginController {
+	@Autowired
+	private JwtServiceImpl jwtService;
+
+	@PostMapping({"/auth/login"})
+	public JwtResponse createJwtToken(@RequestBody LoginRequest loginRequest) throws Exception {
+		return jwtService.createJwtToken(loginRequest);
+	}
 }
