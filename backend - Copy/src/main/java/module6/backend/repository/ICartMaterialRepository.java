d3d61ae9d636692c4f69f6916086b6a3ebe0a48d
Your Name <hoankim1610@gamil.com>
2022-10-17 19:41:51 +0700

    KimPBH up

--- backend - Copy/src/main/java/module6/backend/repository/ICartMaterialRepository.java
+++
@@ -1,9 +0,0 @@
-package module6.backend.repository;
-
-import module6.backend.entity.cart.CartMaterial;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ICartMaterialRepository extends JpaRepository<CartMaterial, Long> {
-}
--- backend - Copy/src/main/java/module6/backend/repository/IEmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package module6.backend.repository;
-
-import module6.backend.entity.employee.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IEmployeeRepository extends JpaRepository<Employee, Long> {
-}
